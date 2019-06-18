eec516ecea8137cb21e0ee0652f374db377fa7fe
shuxia <1347715389@qq.com>
2019-06-18 22:01:10 +0800

    Add .gitignore file

---
+++ DeskBall/OverScene.swift
@@ -0,0 +1,9 @@
+//
+//  OverScene.swift
+//  DeskBall
+//
+//  Created by shuxia on 2019/6/18.
+//  Copyright © 2019 MK. All rights reserved.
+//
+
+import Foundation
---
+++ DeskBall/StartScene.swift
@@ -0,0 +1,9 @@
+//
+//  StartScene.swift
+//  DeskBall
+//
+//  Created by shuxia on 2019/6/17.
+//  Copyright © 2019 MK. All rights reserved.
+//
+
+import Foundation
